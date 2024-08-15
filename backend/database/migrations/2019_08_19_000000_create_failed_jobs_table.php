d3d41c57122006d741bd0093043a5c6240128ef2
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-08-15 08:12:44 +0700

    refactor: :recycle: laravel backend to 10.*

--- backend/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ backend/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chat_histories', function (Blueprint $table) {
+        Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('sender');
-            $table->text('message');
-            $table->timestamps();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
         });
     }
 
@@ -24,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chat_histories');
+        Schema::dropIfExists('failed_jobs');
     }
 };
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,23 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\ChatHistory;
 
-Route::prefix('api')->group(function () {
-    Route::post('/chatbot', function (Request $request) {
-        $message = $request->input('message');
-        $reply = 'Hi! Im Chatbot TCUC!';
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-        ChatHistory::create([
-            'sender' => 'user',
-            'message' => $message,
-        ]);
-
-        ChatHistory::create([
-            'sender' => 'bot',
-            'message' => $reply,
-        ]);
-
-        return response()->json(['reply' => $reply]);
-    });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
